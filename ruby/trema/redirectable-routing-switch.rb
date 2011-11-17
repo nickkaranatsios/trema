b84d25950bc3a1c2f6dc9c3f72e41e7fb565eb65
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-11-17 17:35:37 +0900

    work-backup_11_17

--- ruby/trema/redirectable-routing-switch.rb
+++ ruby/trema/redirectable-routing-switch.rb
@@ -19,6 +19,8 @@ puts "start is called"
     return if message.macda.is_multicast?
     @fdb.learn message.macsa, message.in_port, datapath_id
     if !Authenticator.authenticate_mac( message.macsa )
+      # if the array list is empty call redirect otherwise skip redirection
+      authentication_exempted = AuthenticationFilter.apply( message.packet_info )
     end
   end
 
@@ -49,13 +51,70 @@ puts "start is called"
   end
 
 
-  class ExemptionFilter
-    def initialize packet_info
-      @udp_src_port = packet_info.udp_src_port
-      @udp_dst_port = packet_info.udp_dst_port
-      @tcp_src_port = packet_info.tcp_src_port
-      @tcp_dst_port = packet_info.tcp_dst_port
-      @arp = packet_info.arp?
+  class AuthenticationFilter
+    @subclasses = []
+
+
+    class << self
+      attr_reader :packet_info
+      attr_reader :packet_info_methods
+
+
+      def inherited(subclass)
+        @subclasses << subclass
+      end
+
+
+      def apply_filter packet_info
+        @packet_info_methods = packet_info.class.instance_methods( false )
+        @packet_info = packet_info
+        @subclasses.select { | subclass | subclass.new.allow? }
+      end
+    end
+
+
+    def filter_attributes *attributes
+      attributes.each_index do | i |
+        attribute = attributes[ i ].to_s
+        match = AuthenticationFilter.packet_info_methods.select { | v | v == attribute or v == attribute + "?" }
+        if match.length == 1
+          instance_variable_set("@#{ attributes[ i ] }", AuthenticationFilter.packet_info.send( match.to_s ) )
+          instance_variable_get "@#{ attributes[ i ] }"
+        end
+      end
+    end
+  end
+
+
+
+  class DhcpBootpFilter < AuthenticationFilter
+    def allow?
+      filter_attributes :udp_src_port, :udp_dst_port
+      return @udp_src_port == 67 || @udp_src_port == 68 || @udp_dst_port == 67 || @udp_dst_port == 68
+    end
+  end
+
+
+  class DnsUdpFilter < AuthenticationFilter
+     def allow?
+       filter_attributes :udp_src_port, :udp_dst_port
+       return @udp_src_port == 53 || @udp_dst_port == 53
+     end
+  end
+
+
+  class DnsTcpFilter < AuthenticationFilter
+    def allow?
+      filter_attributes :tcp_src_port, :tcp_dst_port
+      return @tcp_src_port == 53 || @tcp_dst_port == 53
+    end
+  end
+
+
+  class ArpFilter < AuthenticationFilter
+    def allow?
+      filter_attributes :arp
+      return @arp
     end
   end
 end

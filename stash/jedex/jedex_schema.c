dacc1308190a3fabb212c888351054797d8914c5
nickkaranatsios@gmail.com <nickkaranatsios@gmail.com>
2013-06-10 05:39:00 +0900

    home backup

--- stash/jedex/jedex_schema.c
+++ stash/jedex/jedex_schema.c
@@ -21,12 +21,15 @@
 #include "jansson.h"
 #include "../log_writer.h"
 
+#include "priv.h"
 #include "allocation.h"
+#include "basics.h"
 #include "schema_priv.h"
 #include "st.h"
 
 
 #define DEFAULT_TABLE_SIZE 32
+
 
 static void
 jedex_schema_init( jedex_schema *schema, jedex_type type ) {
@@ -53,6 +56,7 @@ is_jedex_id( const char *name ) {
     }
     return 1;
   }
+
   return 0;
 }
 
@@ -81,11 +85,13 @@ jedex_schema_record( const char *name, const char *space ) {
     log_err( "Invalid record schema name %s", name );
     return NULL;
   }
+
   struct jedex_record_schema *record = ( struct jedex_record_schema * ) jedex_new( struct jedex_record_schema );
   if ( record == NULL ) {
-    log_err( "Can not allocate memory for new record schema" );
+    log_err( "Can not allocate new record schema" );
     return NULL;
   }
+
   record->name = strdup( name );
   record->space = space ? strdup( space ) : NULL;
   record->fields = st_init_numtable_with_size( DEFAULT_TABLE_SIZE );
@@ -94,6 +100,7 @@ jedex_schema_record( const char *name, const char *space ) {
     jedex_schema_record_free( record );
     return NULL;
   }
+
   record->fields_byname = st_init_strtable_with_size( DEFAULT_TABLE_SIZE );
   if ( !record->fields_byname ) {
     log_err( "Can not allocate memory for new record schema" );
@@ -103,6 +110,60 @@ jedex_schema_record( const char *name, const char *space ) {
   jedex_schema_init( &record->obj, JEDEX_RECORD );
 
   return &record->obj;
+}
+
+
+size_t
+jedex_schema_record_size( const jedex_schema *record ) {
+  return jedex_schema_to_record( record )->fields->num_entries;
+}
+
+
+jedex_schema *
+jedex_schema_record_field_get( const jedex_schema *record, const char *field_name ) {
+  union {
+    st_data_t data;
+    struct jedex_record_field *field;
+  } val;
+  st_lookup( jedex_schema_to_record( record )->fields_byname, ( st_data_t ) field_name, &val.data );
+
+  return val.field->type;
+}
+
+
+int
+jedex_schema_record_field_get_index( const jedex_schema *record, const char *field_name ) {
+  union {
+    st_data_t data;
+    struct jedex_record_field *field;
+  } val;
+  st_lookup( jedex_schema_to_record( record )->fields_byname, ( st_data_t ) field_name, &val.data );
+
+  return val.field->index;
+}
+  
+
+const char *
+jedex_schema_record_field_name( const jedex_schema *record, int index ) {
+  union {
+    st_data_t data;
+    struct jedex_record_field *field;
+  } val;
+  st_lookup( jedex_schema_to_record( record )->fields, index, &val.data );
+
+  return val.field->name;
+}
+
+
+jedex_schema *
+jedex_schema_record_field_get_by_index( const jedex_schema *record, int index ) {
+  union {
+    st_data_t data;
+    struct jedex_record_field *field;
+  } val;
+  st_lookup( jedex_schema_to_record( record )->fields, index, &val.data );
+
+  return val.field->type;
 }
 
 
@@ -117,6 +178,7 @@ find_named_schemas( const char *name, st_table *st ) {
     return val.schema;
   }
   log_err( "No schema type named %s\n", name );
+
   return NULL;
 }
 
@@ -156,10 +218,10 @@ jedex_type_from_json_t( json_t *json, jedex_type *type,
     *type = JEDEX_BYTES;
   }
   else if ( !strcmp( type_str, "int" ) ) {
-    *type = JEDEX_INT;
+    *type = JEDEX_INT32;
   }
   else if ( !strcmp( type_str, "long" ) ) {
-    *type = JEDEX_LONG;
+    *type = JEDEX_INT64;
   }
   else if ( !strcmp( type_str, "float" ) ) {
     *type = JEDEX_FLOAT;
@@ -196,7 +258,7 @@ jedex_type_from_json_t( json_t *json, jedex_type *type,
 
 jedex_schema *
 jedex_schema_string( void ) {
-  jedex_obj *obj = ( jedex_obj * ) jedex_new( jedex_obj );
+  struct jedex_obj *obj = ( struct jedex_obj * ) jedex_new( struct jedex_obj );
   jedex_schema_init( obj, JEDEX_STRING );
 
   return obj;
@@ -205,7 +267,7 @@ jedex_schema_string( void ) {
 
 jedex_schema *
 jedex_schema_bytes( void ) {
-  jedex_obj *obj = ( jedex_obj * ) jedex_new( jedex_obj );
+  struct jedex_obj *obj = ( struct jedex_obj * ) jedex_new( struct jedex_obj );
   jedex_schema_init( obj, JEDEX_BYTES );
 
   return obj;
@@ -214,7 +276,7 @@ jedex_schema_bytes( void ) {
 
 jedex_schema *
 jedex_schema_int( void ) {
-  jedex_obj *obj = ( jedex_obj * ) jedex_new( jedex_obj );
+  struct jedex_obj *obj = ( struct jedex_obj * ) jedex_new( struct jedex_obj );
   jedex_schema_init( obj, JEDEX_INT32 );
 
   return obj;
@@ -223,7 +285,7 @@ jedex_schema_int( void ) {
 
 jedex_schema *
 jedex_schema_long( void ) {
-  jedex_obj *obj = ( jedex_obj * ) jedex_new( jedex_obj );
+  struct jedex_obj *obj = ( struct jedex_obj * ) jedex_new( struct jedex_obj );
   jedex_schema_init( obj, JEDEX_INT64 );
 
   return obj;
@@ -232,7 +294,7 @@ jedex_schema_long( void ) {
 
 jedex_schema *
 jedex_schema_float( void ) {
-  jedex_obj *obj = ( jedex_obj * ) jedex_new( jedex_obj );
+  struct jedex_obj *obj = ( struct jedex_obj * ) jedex_new( struct jedex_obj );
   jedex_schema_init( obj, JEDEX_FLOAT );
 
   return obj;
@@ -241,7 +303,7 @@ jedex_schema_float( void ) {
 
 jedex_schema *
 jedex_schema_double( void ) {
-  jedex_obj *obj = ( jedex_obj * ) jedex_new( jedex_obj );
+  struct jedex_obj *obj = ( struct jedex_obj * ) jedex_new( struct jedex_obj );
   jedex_schema_init( obj, JEDEX_DOUBLE );
 
   return obj;
@@ -250,7 +312,7 @@ jedex_schema_double( void ) {
 
 jedex_schema *
 jedex_schema_boolean( void ) {
-  jedex_obj *obj = ( jedex_obj * ) jedex_new( jedex_obj );
+  struct jedex_obj *obj = ( struct jedex_obj * ) jedex_new( struct jedex_obj );
   jedex_schema_init( obj, JEDEX_BOOLEAN );
 
   return obj;
@@ -259,7 +321,7 @@ jedex_schema_boolean( void ) {
 
 jedex_schema *
 jedex_schema_null( void ) {
-  jedex_obj *obj = ( jedex_obj * ) jedex_new( jedex_obj );
+  struct jedex_obj *obj = ( struct jedex_obj * ) jedex_new( struct jedex_obj );
   jedex_schema_init( obj, JEDEX_NULL );
 
   return obj;
@@ -270,6 +332,135 @@ jedex_schema *
 jedex_schema_link( jedex_schema *to ) {
   // TODO named linked types not implemented.
   return NULL;
+}
+
+
+jedex_schema *
+jedex_schema_array( const jedex_schema *items ) {
+  struct jedex_schema_array *array = ( struct jedex_schema_array * ) jedex_new( struct jedex_schema_array );
+  if ( !array ) {
+    log_err( "Can not allocate new array schema" );
+    return NULL;
+  }
+
+  array->items = items;
+  jedex_schema_init( array->obj, JEDEX_ARRAY );
+  
+  return &array->obj;
+}
+
+
+jedex_schema *
+jedex_schema_array_items( jedex_schema *array ) {
+  return jedex_schema_to_array( array )->items;
+}
+
+
+jedex_schema *
+jedex_schema_map( const jedex_schema *values ) {
+  struct jedex_schema_map *map = ( struct jedex_schema_map * ) jedex_new( struct jedex_schema_map );
+  if ( !map ) {
+    log_err( "Can not allocate new map schema" );
+    return NULL;
+  }
+  map->values = values;
+  jedex_schema_init( map->obj, JEDEX_MAP );
+
+  return &map->obj;
+}
+
+
+jedex_schema *
+jedex_schema_map( jedex_schema *map ) {
+  return jedex_schema_to_map( map )->values;
+}
+
+
+jedex_schema *
+jedex_schema_union( void ) {
+  struct jedex_schema_union *schema = ( struct jedex_schema_union * ) jedex_new( struct jedex_schema_union );
+  if ( !schema ) {
+    log_err( "Can not allocate new union schema" );
+    return ENOMEM;
+  }
+
+  schema->branches = st_init_numtable_with_size( DEFAULT_TABLE_SIZE );
+  if ( !schema->branches ) {
+    log_err( "Can not allocate new union schema (branches)" );
+    jedex_free( schema );
+    return ENOMEM;
+  }
+
+  schema->branches_byname = st_init_strtable_with_size( DEFAULT_TABLE_SIZE );
+  if ( !schema->branches_byname ) {
+    log_err( "Can not allocate new union schema (branches_byname)" );
+    st_free_table( schema->branches );
+    jedex_free( schema );
+    return ENOMEM;
+  }
+  jedex_schema_init( schema->obj, JEDEX_UNION );
+  
+  return &schema->obj;
+}
+
+
+int
+jedex_schema_union_append( const jedex_schema *union_schema, const jedex_schema *schema ) {
+  check_param( EINVAL, is_jedex_schema( union_schema ), "union schema" );
+  check_param( EINVAL, is_jedex_union( union_schema ), "union schema" );
+  check_param( EINVAL, is_jedex_schema( schema ), "schema" );
+
+  struct jedex_union_schema *unionp = jedex_schema_to_union( union_schema );
+
+  int new_index = unionp->branches->num_entries;
+  st_insert( unionp->branches, new_index, ( st_data_t ) schema );
+
+  const char *name = jedex_schema_type_name( schema );
+  // ?? new_index not schema
+  st_insert( unionp->branches_byname, ( st_data_t ) name, ( st_data_t ) new_index ); 
+
+  return 0;
+}
+
+static int
+save_named_schemas( const char *name, jedex_schema *schema, st_table *st ) {
+  return st_insert( st, ( st_data_t ) name, ( st_data_t ) schema );
+}
+
+
+static int
+jedex_schema_record_field_append( const jedex_schema *record_schema, 
+                                  const char *field_name, 
+                                  jedex_schema *field_schema ) {
+  check_param( EINVAL, is_jedex_schema( record_schema ), "record schema" );
+  check_param( EINVAL, is_jedex_record( record_schema ), "record_schema" );
+  check_param( EINVAL, field_name, "field_name" );
+  check_param( EINVAL, is_jedex_schema( field_schema ), "field_schema" );
+
+  if ( !is_jedex_id( field_name ) ) {
+    log_err( "Invalid jedex identifier" );
+    return EINVAL;
+  }
+
+  if ( record_schema == field_schema ) {
+    log_err( "Can not create a circular schema" );
+    return EINVAL;
+  }
+  
+  struct jedex_record_schema *record = jedex_schema_to_record( record_schema );
+  struct jedex_record_field *new_field = ( struct jedex_record_field * ) jedex_new( struct jedex_record_field );
+  if ( !new_field ) {
+    log_err( "Can not allocate new field" );
+    return ENOMEM;
+  }
+
+  new_field->index = record->fields->num_entries;
+  new_field->name = strdup( field_name );
+  new_field->type = field_schema;
+  st_insert( record->fields, record->fields->num_entries, ( st_data_t ) new_field );
+  st_insert( record->fields_byname, ( st_data_t ) new_field->name, ( st_data_t ) new_field );
+
+  return 0;
 }
 
 
@@ -323,31 +514,37 @@ jedex_schema_from_json_t( json_t *json, jedex_schema **schema, st_table *named_s
         log_err( "Record schema type must have a name" );
         return EINVAL;
       }
+
       if ( !json_is_array( json_fields ) ) {
         log_err( "Record schema type must have fields" );
         return EINVAL;
       }
+
       num_fields = json_array_size( json_fields );
       if ( !num_fields ) {
         log_err( "Record schema type must have at least one field" );
         return EINVAL;
       }
+
       record_name = json_string_value( json_name );
       if ( !record_name ) {
         log_err( "Record schema type must have a name" );
         return EINVAL;
       }
+
       if ( json_is_string( json_namespace ) ) {
         record_namespace = json_string_value( json_namespace );
       }
       else {
         record_namespace = NULL;
       }
+
       *schema = jedex_schema_record( record_name, record_namespace );
       if ( save_named_schemas( record_name, *schema, named_schemas ) ) {
         log_err( "Can not save record schema" );
         return ENOMEM;
       }
+
       for ( i = 0; i < num_fields; i++ ) {
         json_t *json_field = json_array_get( json_fields, i );
         json_t *json_field_name;
@@ -359,14 +556,111 @@ jedex_schema_from_json_t( json_t *json, jedex_schema **schema, st_table *named_s
           log_err( "Record schema field must be an array" );
           return EINVAL;
         }
+
         json_field_name = json_object_get( json_field, "name" );
         if ( !json_field_name ) {
           log_err( "Record field %d must have a name", i );
           return EINVAL;
         }
-        json_field_type = json_object_get( json_field, "type" );
 
+        json_field_type = json_object_get( json_field, "type" );
+        if ( !json_field_type ) {
+          log_err( "Record schema field must have a type" );
+          return EINVAL;
+        }
+
+        field_rval = jedex_schema_from_json_t( json_field_type,
+                                               &json_field_type_schema,
+                                               named_schemas );
+        if ( field_rval ) {
+          return field_rval;
+        }
+
+        field_rval = jedex_schema_record_field_append( *schema,
+                                                       json_string_value,
+                                                       json_field_name,
+                                                       json_field_type_schema );
+        if ( field_rval != 0 ) {
+          return field_rval;
+        }
       }
+    }
+    break;
+    case JEDEX_ARRAY: {
+      json_t *json_items = json_object_get( json, "items" );
+      if ( !json_items ) {
+        log_err( "Array schema must have items" );
+        return EINVAL;
+      }
+
+      jedex_schema *items_schema = NULL;
+      int items_rval;
+
+      items_rval = jedex_schema_from_json_t( json_items,
+                                               &items_schema,
+                                               named_schemas );
+      if ( items_rval ) {
+        return items_rval;
+      }
+      *schema = jedex_schema_array( items_schema );
+    }
+    break;
+    case JEDEX_MAP: {
+      json_t *json_values = json_object_get( json, "values" );
+      if ( !json_values ) {
+        log_err( "Must type must have values" );
+        return EINVAL;
+      }
+
+      jedex_schema *values_schema = NULL;
+      int values_rval;
+
+      values_rval = jedex_schema_from_json_t( json_values,
+                                              &values_schema,
+                                              named_schemas );
+      if ( values_rval ) {
+        return values_rval;
+      }
+      *schema = jedex_schema_map( values_schema );
+    }
+    break;
+    case JEDEX_UNION: {
+      uint32_t num_schemas = json_array_size( json );
+      if ( num_schemas == 0 ) {
+        log_err( "Union type must have at least one branch" );
+        return EINVAL;
+      }
+      *schema = jedex_schema_union();
+      for ( i = 0; i < num_schemas; i++ ) {
+        json_t *schema_json = json_array_get( json, i );
+        if ( !schema_json ) {
+          log_err( "Can not retrieve branch JSON" );
+          return EINVAL;
+        }
+
+        int schema_rval;
+        jedex_schema *s = NULL;
+
+        schema_rval = jedex_schema_from_json_t( schema_json,
+                                                &s,
+                                                named_schemas );
+        if ( schema_rval != 0 ) {
+          return schema_rval;
+        }
+        schema_rval = jedex_schema_union_append( *schema, s );
+        if ( schema_rval != 0 ) {
+          return schema_rval;
+        }
+      }
+    }
+    break;
+    default: {
+      log_err( "Unknown schema type" );
+      return EINVAL;
+    }
+  }
+
+  return 0;
 }
 
 
